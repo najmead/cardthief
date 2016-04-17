b831b0217689d889c7e4de8f7bec7671805ec4a3
najmead <najmead@gmail.com>
2016-04-17 13:40:21 +1000

    Re-organise

--- snippets/dateset.py
+++ snippets/dateset.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from datetime import date, timedelta
+from random import shuffle
 
 conn = sqlite3.connect('netrunner.db')
 
@@ -20,9 +21,14 @@ today = datetime.today()-timedelta(days=1)
 today = today.replace(hour=0, minute=0, second=0, microsecond=0)
 wayBack = wayBack = datetime.strptime('2016-01-27', "%Y-%m-%d")
 
+currentDate = datetime.strftime(datetime.today(), "%Y-%m-%d %H:%M:%S")
+
+print(currentDate)
 d = [today, wayBack]
 
 existing = set(datetime.strptime(dates[x][0],"%Y-%m-%d") for x in range(0, len(dates)))
+
+print(existing)
 
 d.extend(existing)
 
@@ -33,3 +39,8 @@ allDates = set(d[0] + timedelta(x) for x in range((d[-1] - d[0]).days))
 missingDates = sorted(allDates - set(d))
 
 print(missingDates)
+
+shuffle(missingDates)
+
+missingDates[0]
+print(datetime.strftime(missingDates[0], "%Y-%m-%d"))
